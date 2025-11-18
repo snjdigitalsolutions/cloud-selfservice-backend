5ee577d28eceef71ce55608bcbc81f2a7092adcc
Jamey <javarobots74@gmail.com>
2025-11-18 10:08:00 -0500

    WebFlux and ResponseEntityˆ

--- src/main/java/com/snjdigitalsolutions/cloudselfservicebackend/controller/TerraformController.java
+++ src/main/java/com/snjdigitalsolutions/cloudselfservicebackend/controller/TerraformController.java
@@ -6,8 +6,11 @@ import com.snjdigitalsolutions.cloudselfservicebackend.localprocess.TerraformVer
 import com.snjdigitalsolutions.cloudselfservicebackend.response.AddChangeDestroyResponse;
 import com.snjdigitalsolutions.cloudselfservicebackend.response.ProcessResponse;
 import com.snjdigitalsolutions.cloudselfservicebackend.utility.AddChangeDestroyUtility;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class TerraformController {
@@ -25,29 +28,29 @@ public class TerraformController {
     }
 
     @GetMapping("/terraformversion")
-    public ProcessResponse getTerraformVersion() {
+    public Mono<ProcessResponse> getTerraformVersion() {
         terraformVersionProcess.performProcess();
         ProcessResponse response = new ProcessResponse();
         response.setExitCode(terraformVersionProcess.getExitCode());
         response.setStdOut(terraformVersionProcess.getStdOut());
         response.setStdErr(terraformVersionProcess.getStdErr());
         response.setResponse(terraformVersionProcess.getResponse());
-        return response;
+        return Mono.just(response);
     }
 
     @GetMapping("/terraformcdktfversion")
-    public ProcessResponse getCDKTFVersion() {
+    public Mono<ProcessResponse> getCDKTFVersion() {
         cdktfVersionProcess.performProcess();
         ProcessResponse response = new ProcessResponse();
         response.setExitCode(cdktfVersionProcess.getExitCode());
         response.setStdOut(cdktfVersionProcess.getStdOut());
         response.setStdErr(cdktfVersionProcess.getStdErr());
         response.setResponse(cdktfVersionProcess.getResponse());
-        return response;
+        return Mono.just(response);
     }
 
     @GetMapping("/terraformcdktfdiff")
-    public AddChangeDestroyResponse getCDKTFDiff() {
+    public ResponseEntity<Flux<AddChangeDestroyResponse>> getCDKTFDiff() {
         cdktfDiffProcess.performProcess();
         AddChangeDestroyResponse response = new AddChangeDestroyResponse();
         response.setExitCode(cdktfDiffProcess.getExitCode());
@@ -57,7 +60,10 @@ public class TerraformController {
             response.setChange(addChangeDestroyUtility.numberToChange(processResponse).toString());
             response.setDestroy(addChangeDestroyUtility.numberToDestroy(processResponse).toString());
         }
-        return response;
+        Flux<AddChangeDestroyResponse> flux = Flux.just(response);
+        return ResponseEntity.ok()
+                .header("Access-Control-Allow-Origin", "http://localhost:8080")
+                .body(flux);
     }
 
 }
